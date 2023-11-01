e71b19e31175408facd7822e55937c5850192831
Priyanshu Verma <121760896+PvVERM@users.noreply.github.com>
2023-11-01 21:05:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import cv2
 import cvzone
 from cvzone.HandTrackingModule import HandDetector
 import time
+import os  # Import the os module for path operations
+
+# Set the path to the folder containing the PNG images
+image_folder_path = r"C:\Users\premi\Desktop\RPS"
 
 cap = cv2.VideoCapture(0)
 cap.set(3, 640)
@@ -16,19 +20,22 @@ startGame = False
 scores = [0, 0]  # [AI, Player]
 
 while True:
-    imgBG = cv2.imread(r"C:\Users\premi\Downloads\Resources\Resources\BG.png")
+    imgBG = cv2.imread(os.path.join(image_folder_path, "main", "BG.png"))
+
+    if imgBG is None:
+        print("Error: Unable to read the background image.")
+        break
 
     success, img = cap.read()
 
     imgScaled = cv2.resize(img, (0, 0), None, 0.875, 0.875)
     imgScaled = imgScaled[:, 80:480]
 
-    # Find Hands
     hands, img = detector.findHands(imgScaled)  # with draw
 
     if startGame:
 
-        if stateResult is False:
+        if not stateResult:
             timer = time.time() - initialTime
             cv2.putText(imgBG, str(int(timer)), (605, 435), cv2.FONT_HERSHEY_PLAIN, 6, (255, 0, 255), 4)
 
@@ -42,28 +49,35 @@ while True:
                     fingers = detector.fingersUp(hand)
                     if fingers == [0, 0, 0, 0, 0]:
                         playerMove = 1
-                    elif fingers == [1, 1, 1, 1, 1]:
+                    if fingers == [1, 1, 1, 1, 1]:
                         playerMove = 2
-                    elif fingers == [0, 1, 1, 0, 0]:
+                    if fingers == [0, 1, 1, 0, 0]:
                         playerMove = 3
 
                     randomNumber = random.randint(1, 3)
-                    imgAI = cv2.imread(f'Resources/{randomNumber}.png', cv2.IMREAD_UNCHANGED)
+                    img_path = os.path.join(image_folder_path, f"{randomNumber}.png")
+                    
+                    try:
+                        imgAI = cv2.imread(img_path, cv2.IMREAD_UNCHANGED)
+                        if imgAI is None:
+                            raise Exception(f"Unable to read the AI's image '{img_path}'")
+                    except Exception as e:
+                        print(f"Error: {e}")
+                        break
 
-                    if imgAI is not None:
-                        imgBG = cvzone.overlayPNG(imgBG, imgAI, (149, 310))
+                    imgBG = cvzone.overlayPNG(imgBG, imgAI, (149, 310))
 
-                        # Player Wins
-                        if (playerMove == 1 and randomNumber == 3) or \
-                                (playerMove == 2 and randomNumber == 1) or \
-                                (playerMove == 3 and randomNumber == 2):
-                            scores[1] += 1
+                    # Player Wins
+                    if (playerMove == 1 and randomNumber == 3) or \
+                            (playerMove == 2 and randomNumber == 1) or \
+                            (playerMove == 3 and randomNumber == 2):
+                        scores[1] += 1
 
-                        # AI Wins
-                        if (playerMove == 3 and randomNumber == 1) or \
-                                (playerMove == 1 and randomNumber == 2) or \
-                                (playerMove == 2 and randomNumber == 3):
-                            scores[0] += 1
+                    # AI Wins
+                    if (playerMove == 3 and randomNumber == 1) or \
+                            (playerMove == 1 and randomNumber == 2) or \
+                            (playerMove == 2 and randomNumber == 3):
+                        scores[0] += 1
 
     imgBG[234:654, 795:1195] = imgScaled
 
@@ -80,3 +94,9 @@ while True:
         startGame = True
         initialTime = time.time()
         stateResult = False
+
+    elif key == 27:  # Press 'Esc' to exit the program
+        break
+
+cap.release()
+cv2.destroyAllWindows()
